8f0fe9fed591395ecc8cb937a82c6beb6fc13b98
Ricardo Borges <ricardo.borges@somosphi.com>
2020-10-06 20:55:23 -0300

    setup env vars

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase/app'
 import 'firebase/firestore'
 
+require('dotenv').config()
+
 const config = {
-  apiKey: 'AIzaSyBJ8eFFZ6ngXbjzBNOrzklvwpxb6Ri_elQ',
-  authDomain: 'clockwork-3567d.firebaseapp.com',
-  databaseURL: 'https://clockwork-3567d.firebaseio.com',
-  projectId: 'clockwork-3567d',
-  storageBucket: 'clockwork-3567d.appspot.com',
-  messagingSenderId: '397956066617',
-  appId: '1:397956066617:web:2a0bc0499492ea04eecce8',
-  measurementId: 'G-E1BW32CDW6'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(config)
