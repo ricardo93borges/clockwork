a7a421a190bbe775d44f966afc0f0f8be50592ee
Ricardo Borges <ricardo.borges@somosphi.com>
2020-10-14 17:54:20 -0300

    get registers by dates on report page

--- src/components/report/Report.js
+++ src/components/report/Report.js
@@ -1,18 +1,57 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { format, subDays, addDays, differenceInHours } from 'date-fns'
 import { Card, CardContent, Grid } from '@material-ui/core'
 import { KeyboardDatePicker } from '@material-ui/pickers'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
 import FirebaseService from '../../services/firebase'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 
 function Report() {
-  const [startDate, setStartDate] = useState(new Date())
-  const [endDate, setEndDate] = useState(new Date())
-  const [startMaxDate, setStartMaxDate] = useState(new Date())
-  const [endMinDate, setEndMinDate] = useState(new Date())
+  const date = new Date()
+  const [registers, setRegisters] = useState([])
+  const [startDate, setStartDate] = useState(subDays(date, 1))
+  const [endDate, setEndDate] = useState(date)
+  const [startMaxDate, setStartMaxDate] = useState(subDays(date, 1))
+  const [endMinDate, setEndMinDate] = useState(date)
+
+  useEffect(() => {
+    async function getRegistersByDate(startDate, endDate) {
+      const registers = await FirebaseService.getRegistersByDate(startDate, endDate)
+      setRegisters(registers)
+    }
+    getRegistersByDate(startDate, endDate)
+  }, [startDate, endDate])
+
+  const handleStartDateChange = (value) => {
+    const date = new Date(value)
+    setStartDate(date)
+    setEndMinDate(addDays(date, 1))
+  }
+
+  const handleEndDateChange = (value) => {
+    const date = new Date(value)
+    setEndDate(date)
+    setStartMaxDate(subDays(new Date(value), 1))
+  }
+
+  const sumTime = (registers) => {
+    if (registers.length % 2 !== 0) {
+      return 0
+    }
+
+    let time = 0
+    let i = 0
+    while (i < registers.length) {
+      const leftDate = new Date(registers[i])
+      const rightDate = new Date(registers[i + 1])
+      time += Math.abs(differenceInHours(leftDate, rightDate))
+      i += 2
+    }
+    return time
+  }
 
   return (
     <>
@@ -23,29 +62,21 @@ function Report() {
               <Grid item xs={5}>
                 <KeyboardDatePicker
                   margin="normal"
-                  id="time-picker"
                   label="Start date"
                   format="M/D/Y"
                   maxDate={startMaxDate}
                   value={startDate}
-                  onChange={(date, value) => {
-                    console.log(date)
-                    console.log(value)
-                  }}
+                  onChange={(date, value) => handleStartDateChange(value)}
                 />
               </Grid>
               <Grid item xs={5}>
                 <KeyboardDatePicker
                   margin="normal"
-                  id="time-picker"
                   label="End date"
                   format="M/D/Y"
                   minDate={endMinDate}
                   value={endDate}
-                  onChange={(date, value) => {
-                    console.log(date)
-                    console.log(value)
-                  }}
+                  onChange={(date, value) => handleEndDateChange(value)}
                 />
               </Grid>
             </Grid>
@@ -63,10 +94,15 @@ function Report() {
                 </TableRow>
               </TableHead>
               <TableBody>
-                <TableRow>
-                  <TableCell>{'11/10/2020'}</TableCell>
-                  <TableCell>{'8h'}</TableCell>
-                </TableRow>
+                {registers &&
+                  registers.map((register) => (
+                    <TableRow key={register.id}>
+                      <TableCell>
+                        {format(new Date(register.date), 'MM/dd/yyyy')}
+                      </TableCell>
+                      <TableCell>{`${sumTime(register.registers)}h`}</TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
           </CardContent>
